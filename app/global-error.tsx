e6dc33b8dabc21f8fdb1588f14e4bb15f2868512
DjSpykid <opanugaaladetomiwa.com>
2025-01-20 12:49:43 +0100

    commented out the global error file

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,21 +1,21 @@
-"use client";
+// "use client";
 
-import * as Sentry from "@sentry/nextjs";
-import Error from "next/error";
-import { useEffect } from "react";
-export default function GlobalError({ error }: { error: unknown }) {
-    useEffect(() => {
-      if (error instanceof Error) {
-        Sentry.captureException(error);
-      }
-    }, [error]);
+// import * as Sentry from "@sentry/nextjs";
+// import Error from "next/error";
+// import { useEffect } from "react";
+// export default function GlobalError({ error }: { error: unknown }) {
+//     useEffect(() => {
+//       if (error instanceof Error) {
+//         Sentry.captureException(error);
+//       }
+//     }, [error]);
   
-    return (
-      <div>
-        <h1>Something went wrong!</h1>
-        {error instanceof Error && <p>{error.message}</p>}
-      </div>
-    );
-  }
+//     return (
+//       <div>
+//         <h1>Something went wrong!</h1>
+//         {error instanceof Error && <p>{error.message}</p>}
+//       </div>
+//     );
+//   }
   
   
