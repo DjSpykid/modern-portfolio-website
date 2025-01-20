b0d68cad1c2301dc2545e0044047b8634b9a0971
DjSpykid <opanugaaladetomiwa.com>
2025-01-20 12:21:45 +0100

    Fix TypeScript error in GlobalError component

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -4,16 +4,16 @@ import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
 import { useEffect } from "react";
 
-export default function GlobalError({ error }) {
-  useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
-
-  return (
-    <html>
-      <body>
-        <Error />
-      </body>
-    </html>
-  );
-}
+export default function GlobalError({ error }: { error: Error }) {
+    useEffect(() => {
+      Sentry.captureException(error);
+    }, [error]);
+  
+    return (
+      <div>
+        <h1>Something went wrong!</h1>
+        <p>{error.message}</p>
+      </div>
+    );
+  }
+  
