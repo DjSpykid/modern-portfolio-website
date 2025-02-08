71a7fe2cc465cd3519b2da9436674cc72f6a9c14
DjSpykid <opanugaaladetomiwa.com>
2025-02-08 11:37:10 +0100

    making changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,69 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
 
+// import "./globals.css";
+// import { ThemeProvider } from "./provider";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "Daniel's Portfolio",
+//   description: "A Modern Portfolio Web App",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en" suppressHydrationWarning>
+//       <head>
+//         <link rel="icon" href="/myLogo.jpg" sizes="any" />
+//       </head>
+//       <body className={inter.className}>
+//         <ThemeProvider
+//           attribute="class"
+//           defaultTheme="dark"
+//           enableSystem
+//           disableTransitionOnChange
+//         >
+//           {children}
+//         </ThemeProvider>
+//       </body>
+//     </html>
+//   );
+// }
+
+
+
+
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Daniel's Portfolio",
-  description: "A Modern Portfolio Web App",
-};
-
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <title>Daniel Portfolio</title>
+        <meta name="description" content="A Modern Portfolio Web App" />
         <link rel="icon" href="/myLogo.jpg" sizes="any" />
       </head>
       <body className={inter.className}>
-        <ThemeProvider
+        {/* Uncomment the next line if ThemeProvider is working correctly */}
+        {/* <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        > */}
+        {children}
+        {/* </ThemeProvider> */}
       </body>
     </html>
   );
